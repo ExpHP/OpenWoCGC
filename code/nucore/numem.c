a7dc272904cb87e71d97577c9fd723e4651c741f
GoldVisions <58868054+GoldVisions@users.noreply.github.com>
2022-06-17 16:04:12 +0200

    Update numem.c

--- code/nucore/numem.c
+++ code/nucore/numem.c
@@ -1,12 +1,20 @@
 #include "numem.h"
 #include "nuerror.h"
 
-void* memexternal = NULL;
+//void* memexternal = NULL;
 void* memext[2];
-void* highallocaddr = NULL;
-void* peakallocaddr = NULL;
+s32 highallocaddr = 0;
+s32 peakallocaddr = 0;
 u32 totalloc = 0;
 u32 malloced = 0;
+ExternalMemory* memexternal;
+
+
+#define NuError(msg)                                                           \
+    error_func e = NuErrorProlog(__FILE__, __LINE__);                          \
+    e(msg);
+
+#define ROUND_UP(x, align) (((x) + (align)-1) & (-(align)))
 
 void NuMemSetExternal(void* ptr, void* end)
 {
@@ -22,34 +30,54 @@ void NuMemSetExternal(void* ptr, void* end)
 	}
 }
 
-void* NuMemAlloc(size_t size)
-{
-	void* mem = memexternal;
-	if (memexternal == NULL)
-	{
-		totalloc += size;
-		mem = malloc(size);
-		if (mem == NULL)
-		{
-			error_func e = NuErrorProlog("OpenCrashWOC/code/nucore/numem.c", 30);
-			e("NuMemAlloc : Failed to alloc %d bytes!", size);
-		}
-		memset(mem, 0, size);
-		void* end = (void*)((u32)mem + size);
-		if ((u32)highallocaddr < (u32)end)
-		{
-			highallocaddr = end;
-		}
-		if ((u32)peakallocaddr < (u32)end)
-		{
-			peakallocaddr = end;
-		}
-	}
-	else
-	{
-		// HUH???
-	}
-	return mem;
+//void memset(void*, s32, u32); 						//gcc2_compiled__N128
+//void* malloc(u32);                                    //malloc /* extern */
+
+void* NuMemAlloc(size_t size) {
+    void* ret;
+	
+	// Alloc from main heap if possible
+    if (memexternal != NULL) {
+	
+	
+        *memexternal->start = (void*) ROUND_UP((s32) *memexternal->start, 16);
+		
+        if (memexternal->end != NULL && (s32)*memexternal->end - (s32)*memexternal->start < size) {
+            return NULL;
+        }
+        ret = *memexternal->start;
+        *memexternal->start = ((s8*)*memexternal->start) + size;
+        return ret;
+    } 
+	// Main game heap is NULL, fallback on C malloc?
+	else {
+        s32 end;
+		
+		// Total used memory
+        totalloc += size;
+		
+		// Attempt to allocate
+        ret = malloc(size);
+        if (ret == NULL) {
+            NuError("NuMemAlloc : Failed to alloc %d bytes!", size);
+			/*error_func e = NuErrorProlog("OpenCrashWOC/code/nucore/numem.c", 57);
+			e("NuMemAlloc : Failed to alloc %d bytes!", size);*/
+        }
+		
+		// Clear buffer
+        memset(ret, 0, size); // gcc2_compiled__N128 
+		
+		// Resize heap?
+        end = size + (s32)ret;
+        if (end > highallocaddr) {
+            highallocaddr = end;
+        }
+        if (end > peakallocaddr) {
+            peakallocaddr = end;
+        }
+		
+        return ret;
+    }
 }
 
 void NuMemFree(void* data)
