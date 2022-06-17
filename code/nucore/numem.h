25bb3a39aa003f288f90ae5374a5acfa14c86c9b
GoldVisions <58868054+GoldVisions@users.noreply.github.com>
2022-06-17 16:04:32 +0200

    Update numem.h

--- code/nucore/numem.h
+++ code/nucore/numem.h
@@ -2,25 +2,23 @@
 #define NUMEM_H
 
 #include "../types.h"
+#include "nucoretypes.h"
 #include <stdlib.h>
 #include <memory.h>
 
-/*
-  800be5ec 000110 800be5ec  4 NuMemAlloc 	Global
-*/
 
-extern void* memexternal;
+//extern void* memexternal;
 extern void* memext[2];
-extern void* highallocaddr;
-extern void* peakallocaddr;
-extern u32 totalloc;
+extern s32 highallocaddr;
+extern s32 peakallocaddr;
+extern s32 totalloc;
 extern u32 malloced;
 
 // Set memory to be external, with a pointer and ending point.
 void NuMemSetExternal(void* ptr, void* end);
 
 // Allocate memory.
-void* NuMemAlloc(size_t size); // TODO: FINISH NuMemAlloc!!!
+void* NuMemAlloc(size_t size);
 
 // Free memory.
 void NuMemFree(void* data);
